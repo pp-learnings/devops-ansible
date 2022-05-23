ad7e3408656a11354937e5f6004898addba9706c
pp-learnings <105858822+pp-learnings@users.noreply.github.com>
2022-05-23 12:56:52 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-region = "us-east-2"
+region = "us-east-1"
 }
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
-  key_name = "zensar1"
+  key_name = "pp-zen-key"
 
   tags = {
-    Name = "Gagan-devops-server-v2"
+    Name = "pp-devops-server"
     env = "production"
-    owner = "Gagandeep"
+    owner = "ppethe"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
